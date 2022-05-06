77e667934f90cc598e0fcd82fb1362137449a626
Gabriele Cocilovo <99246305+coccigabry@users.noreply.github.com>
2022-05-06 10:41:04 +0200

    game with HTML/CSS graphical front-end

---
+++ numGuesser/script.js
@@ -0,0 +1,33 @@
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+const generateTarget = () => {
+  return Math.floor(Math.random() * 10);
+};
+
+const compareGuesses = (humanGuess, computerGuess, targetGuess) => {
+  const humanDifference = Math.abs(targetGuess - humanGuess);
+  const computerDifference = Math.abs(targetGuess - computerGuess);
+
+  return humanDifference <= computerDifference;
+  /* ovvero:
+  if (humanDiff <= cpuDiff) {
+    return true;
+  } else {
+      return false;
+  }
+  */
+};
+
+const updateScore = winner => {
+  if (winner === 'human') {
+    humanScore++;
+  } else if (winner === 'computer') {
+    computerScore++;
+  }
+};
+
+const advanceRound = () => {
+  currentRoundNumber++;
+};
